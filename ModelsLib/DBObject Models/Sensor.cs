a9a1a63f2858796bae19d89af27571797755ea6d
cristirss <144036@via.dk>
2019-07-23 11:47:32 +0200

    added linq mapping properties to Sensor class

--- ModelsLib/DBObject Models/Sensor.cs
+++ ModelsLib/DBObject Models/Sensor.cs
@@ -2,17 +2,54 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Data.Linq.Mapping;
 using System.Threading.Tasks;
 
 namespace ModelsLib.DBObject_Models
 {
     public class Sensor
     {
-        private SensorType type;
+        private int _id;
+        private string _sens_type;
         private int uptime;
-        private int lifetime;
+        private int lifespan;
+        [Column(IsDbGenerated = true, IsPrimaryKey = true, Storage = "_id")]
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        [Column(Storage = "_sens_type")]
+        public string Sens_type
+        {
+            get { return _sens_type; }
+            set { _sens_type = value; }
+        }
+        [Column(Storage = "uptime")]
+        public int Uptime
+        {
+            get { return uptime; }
+            set { uptime = value; }
+        }
+        [Column(Storage = "lifespan")]
+        public int Lifespan
+        {
+            get { return lifespan; }
+            set { lifespan = value; }
+        }
+        
+        private SensorType type;
+        public SensorType Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
         private List<Reading> readings;
+        public List<Reading> Readings
+        {
+            get { return readings; }
+            set { readings = value; }
+        }
         //private Reading currentReading;
 
         //public Reading CurrentReading
@@ -20,33 +57,13 @@ namespace ModelsLib.DBObject_Models
         //    get { return currentReading; }
         //    set { currentReading = value; }
         //}
-        public List<Reading> Readings
-        {
-            get { return readings; }
-            set { readings = value; }
-        }
-        public int Lifetime
-        {
-            get { return lifetime; }
-            set { lifetime = value; }
-        }
-        public int Uptime
-        {
-            get { return uptime; }
-            set { uptime = value; }
-        }
-        public SensorType Type
-        {
-            get { return type; }
-            set { type = value; }
-        }
 
         public Sensor() { }
         public Sensor(SensorType tp, int ut, int lt)
         {
             type = tp;
             uptime = ut;
-            lifetime = lt; 
+            lifespan = lt; 
         }
         // TODO: constructor with Reading-obj
     }
