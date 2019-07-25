506fbd8aa5b5ecb33f5b9cb36e37537df1f848ce
cristirss <144036@via.dk>
2019-07-25 21:11:49 +0200

    Added DBHelper class

---
+++ ModelsLib/Helpers/DBHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Linq;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ModelsLib
+{
+    internal class DBHelper
+    {
+        private static readonly string connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+        private static readonly DataContext DataContext = new DataContext(connectionString);
+
+        internal static T SelectOne<T>(Expression<Func<T, bool>> filter) where T: class
+        {
+            return DataContext.GetTable<T>().Where(filter).FirstOrDefault();
+        }
+
+        internal static void Insert<T>(T obj) where T : class
+        {
+            DataContext.GetTable<T>().InsertOnSubmit(obj);
+            DataContext.SubmitChanges();
+        }
+
+        internal static void Update<T>(Expression<Func<T, bool>> filter, T obj) where T : class
+        {
+            var matchedObj = SelectOne(filter); 
+            Utils.CopyPropertiesTo(obj, matchedObj);
+            DataContext.SubmitChanges();
+        }
+
+        internal static void Delete<T>(Expression<Func<T, bool>> filter) where T : class
+        {
+            DataContext.GetTable<T>().DeleteOnSubmit(SelectOne(filter));
+            DataContext.SubmitChanges(); 
+        }
+
+        internal static List<T> SelectAll<T>(Expression<Func<T, bool>> filter) where T : class
+        {
+            return DataContext.GetTable<T>().Where(filter).ToList();
+        }
+
+    }
+}
--- ModelsLib/Helpers/Utils.cs
+++ ModelsLib/Helpers/Utils.cs
@@ -32,5 +32,26 @@ namespace ModelsLib
             return dtDateTime;
         }
 
+        public static void CopyPropertiesTo<T, TU>(this T source, TU dest)
+        {
+            var sourceProps = typeof(T).GetProperties().Where(x => x.CanRead).Skip(1).ToList();
+            var destProps = typeof(TU).GetProperties()
+                    .Where(x => x.CanWrite)
+                    .ToList();
+
+            foreach (var sourceProp in sourceProps)
+            {
+                if (destProps.Any(x => x.Name == sourceProp.Name))
+                {
+                    var p = destProps.First(x => x.Name == sourceProp.Name);
+                    if (p.CanWrite)
+                    {
+                        p.SetValue(dest, sourceProp.GetValue(source, null), null);
+                    }
+                }
+
+            }
+
+        }
     }
 }
